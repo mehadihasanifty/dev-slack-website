03078a634fee918fce8528288821da1201a2971e
mehedihasanifty <mh3915287@gmail.com>
2026-09-12 17:19:14 +0600

    re implemnt re structure of this project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import Footer from './components/Footer';
-import HeroSection from './components/heroSection';
-import Nav from './components/Nav';
-import TechnologyPart from './components/TechnologyPart';
+import Footer from "./components/Footer";
+import HeroSection from "./components/heroSection";
+import Nav from "./components/Nav";
+import TechnologyPart from "./components/TechnologyPart";
 function App() {
-
   return (
-    <>
+    <div className="App">
       <Nav />
-      <HeroSection />
-      <TechnologyPart />
+      <div className="max-w-11/12 mx-auto">
+        <HeroSection />
+        <TechnologyPart />
+      </div>
       <Footer />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
